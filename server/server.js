d55394be3e083b0b158743ff42d26e17e8e64924
riojack <ayjay122@gmail.com>
2018-04-25 07:13:24 -0500

    Change server splash message

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express'),
-  RootRoutes = require('./root.route');
+  RootRoutes = require('./root.route'),
+
+  PORT = 3320;
 
 const app = express();
 
 app.use('/', RootRoutes.configure());
 
-app.listen(3320, () => console.log('Started server'));
+app.listen(PORT, () => console.log(`It's working! IT'S WORKING!\nOpen yer browser: http://localhost:${PORT}/`));
