4fca88f92475ac8f15a24d8d87482950e76a2a08
riojack <ayjay122@gmail.com>
2018-04-27 12:10:12 -0500

    Have Jest stub SCSS imports before running tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   setupFiles: [
     '<rootDir>/test-helper.js'
   ],
+  moduleNameMapper: {
+    '\\.(css|scss)$': '<rootDir>/scss-stub.js'
+  },
   moduleFileExtensions: ['jsx', 'js'],
   testMatch: ['**/test/**/*.+(js)'],
   verbose: true
