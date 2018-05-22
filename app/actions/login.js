1c75dab5f5185e60ff34ab7a7d9420a1e58d97a7
riojack <ayjay122@gmail.com>
2018-05-22 17:49:29 -0500

    Light coverage around tryLogin action shape

--- app/actions/login.js
+++ app/actions/login.js
@@ -1,5 +1,5 @@
 export const TRY_LOGIN = 'TRY_LOGIN';
 
-export function tryLogin(username, password) {
+export default function tryLogin(username, password) {
   return { type: TRY_LOGIN, username, password };
 }
---
+++ test/actions/login.test.js
@@ -0,0 +1,11 @@
+import tryLogin from "../../app/actions/login";
+
+describe('Login action', () => {
+  it('should create an action with type "TRY_LOGIN" and also includes the username and password', () => {
+    let loginAction = tryLogin('billybob', 'abc123');
+
+    expect(loginAction).toHaveProperty('type', 'TRY_LOGIN');
+    expect(loginAction).toHaveProperty('username', 'billybob');
+    expect(loginAction).toHaveProperty('password', 'abc123');
+  });
+});
