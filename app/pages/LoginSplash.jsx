a43e4de075334903c27b83f3a13050ee32d74776
riojack <ayjay122@gmail.com>
2018-07-08 18:41:11 -0500

    Test that props.handleLogin is called when 'Login' is clicked

--- app/pages/LoginSplash.jsx
+++ app/pages/LoginSplash.jsx
@@ -1,15 +1,37 @@
 import * as React from 'react';
+import tryLogin from '../actions/login';
 
-export default class LoginSplash extends React.Component {
+class LoginSplashComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {username: '', password: ''};
+  }
+
   componentDidMount() {
     require('./login-splash.scss');
   }
 
+  captureEmail(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  capturePassword(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  triggerLogin() {
+    this.props.handleLogin(this.state.username, this.state.password);
+  }
+
   render() {
     return <div class="login-form">
-      <input type="email" placeholder="E-mail" />
-      <input type="password" placeholder="Password" />
-      <button>Login</button>
+      <input type="email" placeholder="E-mail" onChange={this.captureEmail.bind(this)} />
+      <input type="password" placeholder="Password" onChange={this.capturePassword.bind(this)} />
+      <button onClick={this.triggerLogin.bind(this)}>Login</button>
     </div>;
   }
 }
+
+export {
+  LoginSplashComponent
+};
--- test/unit/pages/LoginSplash.test.js
+++ test/unit/pages/LoginSplash.test.js
@@ -1,10 +1,11 @@
 import * as enzyme from 'enzyme';
-import LoginSplash from '../../../app/pages/LoginSplash';
+import {LoginSplashComponent} from '../../../app/pages/LoginSplash';
 import * as React from 'react'
+import tryLogin from '../../../app/actions/login';
 
 describe('Login Splash Page', () => {
   it('should have an email field and password field', () => {
-    let splash = enzyme.shallow(<LoginSplash />);
+    let splash = enzyme.shallow(<LoginSplashComponent />);
 
     let emailField = splash.find('input[type="email"]');
     let passwordField = splash.find('input[type="password"]');
@@ -14,11 +15,32 @@ describe('Login Splash Page', () => {
   });
 
   it('should have a button with the word "Login"', () => {
-    let splash = enzyme.shallow(<LoginSplash />);
+    let splash = enzyme.shallow(<LoginSplashComponent />);
 
     let loginButton = splash.find('button');
 
     expect(loginButton).toHaveLength(1);
     expect(loginButton.text()).toEqual('Login');
+  });
+
+  it('should trigger props.handleLogin when the "Login" button is clicked', () => {
+    const username = 'me@myself.org';
+    const password = 'abc 123';
+
+    const handleLoginStub = jest.fn();
+
+    let splash = enzyme.shallow(<LoginSplashComponent handleLogin={handleLoginStub} />);
+
+    let emailField = splash.find('input[type="email"]');
+    let passwordField = splash.find('input[type="password"]');
+
+    let loginButton = splash.find('button');
+
+    emailField.simulate('change', { target: {value: username}});
+    passwordField.simulate('change', { target: {value: password}});
+
+    loginButton.simulate('click');
+
+    expect(handleLoginStub).toHaveBeenCalledWith(username, password);
   });
 });
