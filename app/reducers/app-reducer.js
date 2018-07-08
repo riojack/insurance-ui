393b4e0a2bd42c98a0c432adf48beefce6e3510c
riojack <ayjay122@gmail.com>
2018-07-07 19:13:03 -0500

    Logging in should push the state.logged_in to true

--- app/reducers/app-reducer.js
+++ app/reducers/app-reducer.js
@@ -1,8 +1,16 @@
+import { TRY_LOGIN } from "../actions/login";
 
 const INIT_STATE = {
   logged_in: false
 };
 
 export default function appReducer(state = INIT_STATE, action) {
+  if (!action) {
+    return state;
+  }
+  
+  if (action.type === TRY_LOGIN) {
+    return Object.assign({}, state, {logged_in: true});
+  }
   return Object.assign({}, state);
 }
--- test/reducers/app-reducer.test.js
+++ test/reducers/app-reducer.test.js
@@ -1,9 +1,16 @@
 import appReducer from '../../app/reducers/app-reducer';
+import tryLogin from '../../app/actions/login';
 
 describe('App Reducer', () => {
   it('should not be logged in in the default state', () => {
     let state = appReducer();
 
     expect(state).toHaveProperty('logged_in', false);
+  });
+
+  it('should be logged in in the next state', () => {
+    let state = appReducer(undefined, tryLogin('floopy', 'boopy'));
+
+    expect(state).toHaveProperty('logged_in', true);
   });
 });
